95fb9e7e686259452e66ee1ab44d6b1daebd745c
Boyeon-Shin <anna007096@gmail.com>
2024-03-23 22:21:45 +0900

    Ch03 study

---
+++ Ch03/3.6/readFilePromise.js
@@ -0,0 +1,11 @@
+import fs from 'fs/promises';
+// import { readFile } from 'fs/promises'
+
+fs.readFile('./readme.txt')
+    .then((data) => {
+        console.log(data);
+        console.log(data.toString());
+    })
+    .catch((err) => {
+        console.error(err);
+    });
